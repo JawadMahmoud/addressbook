1629d5c4717cc97ccb435b3eb62d24a249ec17b3
Jawad Mahmoud <jd.mahmoud@gmail.com>
2019-04-11 21:31:37 +0100

    remove unnecessary files, edit gitignore

--- contacts_project/contacts_project/urls.py
+++ contacts_project/contacts_project/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from contactsapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contacts/', include('contactsapp.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
